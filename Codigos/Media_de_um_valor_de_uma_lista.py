d41ee9614ce99885fe15342a001e554f954f38f1
Davyd Braz <102470929+DavydBraz@users.noreply.github.com>
2022-12-11 20:56:13 -0300

    Update Media_de_um_valor_de_uma_lista.py

--- Codigos/Media_de_um_valor_de_uma_lista.py
+++ Codigos/Media_de_um_valor_de_uma_lista.py
@@ -3,7 +3,7 @@ resultado=0
 
 for i in range(casos):
     resultado=0
-    avg=list(map(int, input("Digite os valores que deseja e 0 para indicar ultimo valor"). split()))
+    avg=list(map(int, input("Digite os valores que deseja e 0 para indicar ultimo valor: "). split()))
     for numero in avg:
         if numero!=0:
             resultado+=numero
